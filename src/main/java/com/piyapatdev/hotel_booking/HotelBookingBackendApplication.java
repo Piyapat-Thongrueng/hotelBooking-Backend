3f7d0dbb6e2da695032af7a0460284302170d629
Piyapat <piyapat.thongrueng@gmail.com>
2025-06-03 07:17:02 +0700

    complete create project and packages structure

---
+++ src/main/java/com/piyapatdev/hotel_booking/HotelBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piyapatdev.hotel_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingBackendApplication.class, args);
+	}
+
+}
